2773b7447877005784999cb3e56aa7db19e139ce
Jason Belgard <belgard.jason@gmail.com>
2021-01-07 22:52:27 -0600

    Day 1

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,18 @@
+/*
+let country = 'United States of America';
+let continent = 'North America';
+let population = '328 million';
+let isIsland = false;
+let language;
+
+console.log (country);
+console.log (continent);
+console.log (population);
+console.log (typeof isIsland);
+console.log (typeof population);
+console.log (typeof country);
+console.log (typeof language);
+*/
+
+
+
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,30 @@
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+
+// console.log('Jonas');
+// console.log(23);
+
+// let firstName = 'Matilda';
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
